68ee7fdfa23f3a9939abb302713ccf30b4914b2e
RZFeeser <rzfeeser@users.noreply.github.com>
2023-10-16 17:27:43 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Fill in your doc-string
+
+"""
+
+import zachwork
+import larrywork
+
+
+def main():
+    networkdata = zachwork.networkreader()
+    print(networkdata)
+# if the script is called via# python3 name_of_script.py
+# then run main()
+if __name__ == "__main__":
+    main()
